152bc0aa0693cd4a9886fe46232227459b722a95
mimura15 <102932331+mimura15@users.noreply.github.com>
2022-04-22 03:38:26 +0000

    [Add] controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
   root to: 'homes#top'
   get 'homes/about' => 'homes#about', as: 'about'
   devise_for :users
