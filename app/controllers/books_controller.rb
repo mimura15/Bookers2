9df1bffd667b22f0506c938a0394338e1a257a1b
mimura15 <102932331+mimura15@users.noreply.github.com>
2022-04-25 08:27:21 +0000

    [Update] 投稿機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,4 +7,18 @@ class BooksController < ApplicationController
 
   def edit
   end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
+  end
+
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,8 +5,14 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books = @user.books
+    @book = Book.new
   end
 
   def edit
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:image)
   end
 end
