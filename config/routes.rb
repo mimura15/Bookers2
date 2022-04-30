f2d2168477ecaf87e17c4113dbceca74dc774d7a
mimura15 <102932331+mimura15@users.noreply.github.com>
2022-04-30 11:37:36 +0000

    [Fix] delete機能

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :show, :edit, :update]
-  resources :books, only: [:index, :show, :edit, :create, :destroy]
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  resources :books, only: [:index, :show, :edit, :create]
 end
