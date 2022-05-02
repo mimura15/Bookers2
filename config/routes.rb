365f17ff1746ff878c74d6b39c9c6b8c004496e8
mimura15 <102932331+mimura15@users.noreply.github.com>
2022-05-02 08:22:31 +0000

    [Uproad] book バリデーション

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :show, :edit, :update]
   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
-  resources :books, only: [:index, :show, :edit, :create]
+  resources :books, only: [:index, :show, :edit, :create, :update]
 end
