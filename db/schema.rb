fbb53cc59cdb63dfc278c3628333b1760f73ef81
mimura15 <102932331+mimura15@users.noreply.github.com>
2022-05-09 05:18:39 +0000

    [Fix] マイグレーションファイル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_23_085045) do
+ActiveRecord::Schema.define(version: 2022_05_09_051648) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
